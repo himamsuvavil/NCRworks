3899613c5c09a64343efbc3a5bfe54e4a37419c2
cdac <cdac@DESKTOP-SS5H7HT>
2019-02-12 15:36:10 -0800

    changed matching word

--- miniproject1/miniproject1/match_word.c
+++ miniproject1/miniproject1/match_word.c
@@ -20,12 +20,12 @@ int rotate(char *fp,char *s)
 		a[i] = '\0';
 		if (strcmp(a, s) == 0)
 		{
-			printf("%s", strrev(a));
-			/*strcpy_s(a,sizeof(s)+1 ,s + 1);
+			//printf("%s", strrev(a));
+			strcpy_s(a,sizeof(s)+1 ,s + 1);
 			l[0] = s[0];
 			l[1] = '\0';
 			strcat_s(a,sizeof(s)+1,  l);
-			strcpy_s(s, sizeof(a)+1, a);*/
+			strcpy_s(s, sizeof(a)+1, a);
 		}
 		else
 		{
