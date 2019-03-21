fa72ee455bad6d76ccd973ba65eda3d15e3e0c9b
“BillakantiShirisha” <billakantishirisha@gmail.com>
2019-03-21 12:44:48 +0530

    stack and queue from dQueue

--- AdvancedCPP1/Dqueue1/Dqueue.cpp
+++ AdvancedCPP1/Dqueue1/Dqueue.cpp
@@ -15,6 +15,7 @@ public:
 	{
 		rear = front = NULL;
 	}
+	/*-----Add at the back end------*/
 	int insertRear(int elem)
 	{
 		struct doubleLinkedList *insertdata = NULL;
@@ -40,6 +41,7 @@ public:
 		}
 		return -1;
 	}
+	/*-------Add at the beginning---------*/
 	int insertFront(int elem)
 	{
 
@@ -66,6 +68,7 @@ public:
 		}
 		return -1;
 	}
+	/*-------Delete at the back end----------*/
 	int deleteRear()
 	{
 		int deleted = -1;
@@ -80,7 +83,7 @@ public:
 		{
 			
 			front = rear = NULL;
-			//free deleteNode;
+			
 		}
 		else
 		{
@@ -88,8 +91,10 @@ public:
 			rear = rear->prev;
 			deleteNode->prev = NULL;
 		}
+		delete deleteNode;
 		return deleted;
 	}
+	/*---------Delete at the beginning-------*/
 	int deleteFront()
 	{
 		int deleted = -1;
@@ -104,7 +109,7 @@ public:
 		{
 
 			front = rear = NULL;
-			//free deleteNode;
+			
 		}
 		else
 		{
@@ -112,6 +117,7 @@ public:
 			front = front->prev;
 			deleteNode->next = NULL;
 		}
+		delete deleteNode;
 		return deleted;
 	}
 	~dQueue()
@@ -119,12 +125,132 @@ public:
 		
 	}
 };
-class Stack :public dQueue
+
+/*--------Add at rear end & delete at rear end------------*/
+
+class stack :public dQueue
 {
-	struct doubleLinkedList *top;
+	
 public:
-	Stack()
+	stack()
+	{}
+	/*------Displays from first element added to stack to top element-------*/
+	int displayStack()
 	{
-		top = NULL;
+		struct doubleLinkedList *currentNode = front;
+		if (rear == NULL&&front == NULL)
+		{
+			cout << "Empty Stack\n";
+			return -1;
+		}
+		while (currentNode != NULL)
+		{
+			cout << currentNode->data << " ";
+			currentNode = currentNode->next;
+		}
+		cout << endl;
+		return 0;
 	}
 };
+
+/*----------Adds at front end and deletes at rear end------------*/
+
+class queue :public dQueue
+{
+public:
+	queue()
+	{
+
+	}
+	/*--------Displays from front end to rear end-------*/
+	int displayQueue()
+	{
+		struct doubleLinkedList *currentNode = front;
+		if (rear == NULL&&front == NULL)
+		{
+			cout << "Empty Queue\n";
+			return -1;
+		}
+		while (currentNode != rear)
+		{
+			cout << currentNode->data << " ";
+			currentNode = currentNode->next;
+		}
+		cout << currentNode->data << endl;
+		return 0;
+	}
+};
+int main()
+{
+	stack stackObj;
+	int stackChoice;
+	int insertElement, deletedElement;
+	int checkInsert, checkDisplay;
+	cout << "*-----Performing operations on Stack-----*\n\n";
+	do {
+		cout << "Enter your choice:";
+		cout << "1.Push\t2.Pop\t3.Display\t4.Exit\n";
+		cin >> stackChoice;
+		switch (stackChoice)
+		{
+		case 1:cout << "Enter element to be pushed into stack\n";
+			cin >> insertElement;
+			checkInsert = stackObj.insertFront(insertElement);
+			if (checkInsert == -1)
+				cout << "\nFailed to insert\n\n";
+			else
+				cout << "\nInserted Successfully\n\n";
+			break;
+		case 2:deletedElement = stackObj.deleteRear();
+			if (deletedElement == -1)
+				cout << "\nFailed to delete from Stack\n\n";
+			else
+				cout << "\nDelted element from Stack is " << deletedElement << endl << endl;
+			break;
+		case 3:cout << "\nStack Element are displayed from (bottom to top) are :" <<endl;
+			checkDisplay = stackObj.displayStack();
+			if (checkDisplay == -1)
+				cout << "\nFailed to display elements of Stack\n\n";		
+			break;
+		case 4:
+		default:
+			break;
+		}
+	} while (stackChoice == 1 || stackChoice == 2 || stackChoice == 3);
+
+	queue queueObj;
+	int queueChoice;
+	cout << "*-----Performing operations on Queue-----*\n\n";
+	do {
+		cout << "Enter your choice:";
+		cout << "1.Add element\t2.Delete element\t3.Display\t4.Exit\n";
+		cin >> queueChoice;
+		switch (queueChoice)
+		{
+		case 1:cout << "Enter element to be pushed into stack\n";
+			cin >> insertElement;
+			checkInsert =queueObj.insertRear(insertElement);
+			if (checkInsert == -1)
+				cout << "\nFailed to insert\n\n";
+			else
+				cout << "\nInserted Successfully\n\n";
+			break;
+		case 2:deletedElement = queueObj.deleteRear();
+			if (deletedElement == -1)
+				cout << "\nFailed to delete from Stack\n\n";
+			else
+				cout << "\nDelted element from Stack is " << deletedElement << endl << endl;
+			break;
+		case 3:cout << "\nStack Element are displayed from (bottom to top) are :" << endl;
+			checkDisplay=queueObj.displayQueue();
+			if (checkDisplay == -1)
+				cout << "\nFailed to display elements of Stack\n\n";				
+			break;
+		case 4:
+		default:
+			break;
+		}
+	} while (queueChoice == 1 || queueChoice == 2 || queueChoice == 3);
+
+	return 0;
+}
